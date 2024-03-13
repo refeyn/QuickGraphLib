b36abfd3b9a90db73e892891970d8813275d21ae
matsjoyce <matthew.joyce@refeyn.com>
2024-03-13 16:29:26 +0000

    Add QML import path helper

--- quickgraphlib_helpers/__init__.py
+++ quickgraphlib_helpers/__init__.py
@@ -1,4 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 Matthew Joyce and other QuickGraphLib contributors
 # SPDX-License-Identifier: MIT
 
+import pathlib
+
 from . import contours, export
+
+QML_IMPORT_PATH = str(pathlib.Path(__file__).parent.parent)
