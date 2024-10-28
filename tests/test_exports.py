5ac35940ca9807a77875d65dc5c88cf389449d65
matsjoyce <matthew.joyce@refeyn.com>
2024-10-28 17:00:50 +0000

    Text exporting with Helpers

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -5,10 +5,10 @@ import pathlib
 
 import numpy as np
 import pytest
-from PySide6 import QtCore, QtGui, QtQml
+from PySide6 import QtCore, QtGui, QtQml, QtQuick, QtQuickControls2
 
 import QuickGraphLib
-from examples import conway
+from examples import conway  # pylint: disable=unused-import
 
 EXAMPLE_PATHS = [
     path
@@ -16,50 +16,114 @@ EXAMPLE_PATHS = [
     if path.name != "ExampleGallery.qml"
 ]
 REFERENCE_PATH = pathlib.Path(__file__).parent / "reference_images"
+FONT_PATH = pathlib.Path(__file__).parent / "fonts"
 GENERATE_REFERENCE_IMAGES = False
+
+QML_IMPORT_NAME = "Tests"
+QML_IMPORT_MAJOR_VERSION = 1
 
 
 @pytest.fixture(scope="session")
 def qapp() -> QtGui.QGuiApplication:
-    return QtGui.QGuiApplication([])
+    QtQuick.QQuickWindow.setGraphicsApi(
+        QtQuick.QSGRendererInterface.GraphicsApi.Software
+    )
+
+    QtQuickControls2.QQuickStyle.setStyle("Basic")
+
+    app = QtGui.QGuiApplication(["", "-platform", "offscreen"])
+
+    for fontPath in FONT_PATH.glob("*.ttf"):
+        QtGui.QFontDatabase.addApplicationFont(str(fontPath))
+    app.setFont(QtGui.QFont("DejaVuSans", 11, QtGui.QFont.Weight.Normal))
+
+    return app
+
+
+@QtQml.QmlElement
+@QtQml.QmlSingleton
+class PictureSaver(QtCore.QObject):
+    @QtCore.Slot(QtGui.QPicture, QtCore.QUrl)
+    def savePicture(self, picture: QtGui.QPicture, url: QtCore.QUrl) -> None:
+        picture.save(url.toLocalFile())
+
+
+def compare_images(a: pathlib.Path, b: pathlib.Path, width: int, height: int) -> float:
+    a_img = QtGui.QImage(str(a))
+    b_img = QtGui.QImage(str(b))
+    assert a_img.width() == b_img.width() == width
+    assert a_img.height() == b_img.height() == height
+    shape = (height, width, 4)
+    reference_arr = np.asarray(a_img.bits()).reshape(shape).astype(int)
+    output_arr = np.asarray(b_img.bits()).reshape(shape).astype(int)
+    difference = np.abs(reference_arr - output_arr).sum() / reference_arr.size / 255
+    print(f"Difference of {a.name} is {difference:.4f}")
+    return difference
 
 
 @pytest.mark.parametrize("example_path", EXAMPLE_PATHS, ids=lambda p: str(p.stem))
 def test_export(
     example_path: pathlib.Path, qapp: QtGui.QGuiApplication, tmp_path: pathlib.Path
 ) -> None:
-    image_path = tmp_path / f"{example_path.stem}.png"
+    grab_path = tmp_path / f"{example_path.stem}_grab.png"
+    png_path = tmp_path / f"{example_path.stem}.png"
+    svg_path = tmp_path / f"{example_path.stem}.svg"
+    picture_path = tmp_path / f"{example_path.stem}.dat"
+
     engine = QtQml.QQmlApplicationEngine()
     engine.setInitialProperties(
         {
             "exampleUrl": QtCore.QUrl.fromLocalFile(example_path),
-            "outputUrl": QtCore.QUrl.fromLocalFile(image_path),
+            "outputGrabUrl": QtCore.QUrl.fromLocalFile(grab_path),
+            "outputPngUrl": QtCore.QUrl.fromLocalFile(png_path),
+            "outputSvgUrl": QtCore.QUrl.fromLocalFile(svg_path),
+            "outputPictureUrl": QtCore.QUrl.fromLocalFile(picture_path),
         }
     )
     engine.addImportPath(QuickGraphLib.QML_IMPORT_PATH)
     engine.loadData(
         rb"""
 import QtQuick
+import QuickGraphLib as QuickGraphLib
+import Tests as Tests
 Window {
     id: root
     required property url exampleUrl
-    required property url outputUrl
+
+    required property url outputGrabUrl
+    required property url outputPngUrl
+    required property url outputSvgUrl
+    required property url outputPictureUrl
+    property bool hasExported: false
+
     width: 800
     height: 600
     visible: true
-    Loader {
-        id: loader
-        source: exampleUrl
+    Rectangle {
+        id: content
+        color: "white"
         anchors.fill: parent
+        border.width: 0
+
+        Loader {
+            source: exampleUrl
+            anchors.fill: parent
+        }
     }
-    Component.onCompleted: {
-        let res = loader.item.grabToImage(result => {
-           result.saveToFile(root.outputUrl);
+    onFrameSwapped: {
+        if (root.hasExported) return;
+        content.ensurePolished();
+        let res = content.grabToImage(result => {
+            result.saveToFile(root.outputGrabUrl);
             Qt.exit(0);
         });
         if (!res) {
             Qt.exit(1);
         }
+        QuickGraphLib.Helpers.exportToPng(content, root.outputPngUrl);
+        QuickGraphLib.Helpers.exportToSvg(content, root.outputSvgUrl);
+        Tests.PictureSaver.savePicture(QuickGraphLib.Helpers.exportToPicture(content), root.outputPictureUrl);
+        root.hasExported = true;
     }
 }
 """
@@ -68,20 +132,17 @@ Window {
     engine.quit.connect(qapp.quit)
     assert qapp.exec() == 0
 
-    reference_path = REFERENCE_PATH / image_path.name
     if GENERATE_REFERENCE_IMAGES:
-        if reference_path.exists():
-            reference_path.unlink()
-        image_path.rename(reference_path)
+        for fname in [grab_path, png_path, svg_path, picture_path]:
+            if (reference_path := (REFERENCE_PATH / fname.name)).exists():
+                reference_path.unlink()
+            fname.rename(reference_path)
 
     else:
-        reference_img = QtGui.QImage(str(reference_path))
-        output_img = QtGui.QImage(str(image_path))
-        assert reference_img.width() == output_img.width() == 800
-        assert reference_img.height() == output_img.height() == 600
-        shape = (reference_img.height(), reference_img.width(), 4)
-        reference_arr = np.asarray(reference_img.bits()).reshape(shape).astype(int)
-        output_arr = np.asarray(output_img.bits()).reshape(shape).astype(int)
-        difference = np.abs(reference_arr - output_arr).sum() / shape[0] / shape[1]
-        print(difference)
-        assert difference < 0.01
+        assert (
+            compare_images(grab_path, REFERENCE_PATH / grab_path.name, 800, 600) < 0.001
+        )
+        assert (
+            compare_images(png_path, REFERENCE_PATH / png_path.name, 1600, 1200) < 0.001
+        )
+        # TODO SVG and QPicture comparisons
